57bc1129b2862073fc50d9c1b33a1c032643c402
penguinboi <simon.roy1211@gmail.com>
2025-04-21 17:12:56 -0400

    Improved README and added project info in mix.exs

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule Silk.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/PenguinBoi12/silk"
+  @version "0.1.0"
+
   def project do
     [
-      app: :silk,
-      version: "0.1.0",
+      app: :silk_html,
+      version: @version,
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      docs: docs()
     ]
   end
 
@@ -23,6 +29,32 @@ defmodule Silk.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description() do
+    """
+    Silk is a lightweight Elixir DSL for generating HTML in a clean, expressive, and composable 
+    way - using just Elixir syntax.
+    """
+  end
+
+  defp package() do
+    [
+      maintainers: ["Simon Roy"],
+      licenses: ["GPL-3.0"],
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "readme",
+      name: "Silk",
+      source_ref: "v#{@version}",
+      canonical: "http://hexdocs.pm/silk",
+      source_url: @source_url,
+      extras: ["README.md", "LICENSE"]
     ]
   end
 end
